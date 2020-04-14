3c5f7f8c00c1e6d4a7ff4a793ea5d0c65e0d5f2f
Ajay <ajayvishwanath.94@gmail.com>
2020-04-13 17:31:13 -0700

    binary tree work

---
+++ serialize_deserialize_binary_tree.js
@@ -0,0 +1,26 @@
+var serialize = function (root) {
+    if (root === null) return 'null';
+
+    let left = serialize(root.left);
+    let right = serialize(root.right);
+
+    return root.val.toString() + "," + left + "," + right
+};
+
+var deserialize = function (data) {
+    if (data === null) return null;
+    const queue = data.split(',');
+    return deserializeHelper(queue);
+};
+
+var deserializeHelper = function (queue) {
+    if (queue.length < 1) return;
+    let nodeValue = queue.shift();
+    if (nodeValue === 'null') return null;
+
+    let newNode = new TreeNode(parseInt(nodeValue));
+    newNode.left = deserializeHelper(queue);
+    newNode.right = deserializeHelper(queue);
+
+    return newNode
+}
