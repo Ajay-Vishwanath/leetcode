9640845734c0c8885aa944941cb210f36c796ef5
Ajay <ajayvishwanath.94@gmail.com>
2020-04-20 12:16:06 -0700

    Linked List Cycle 2nd Implementation

--- linked_list_cycle.js
+++ linked_list_cycle.js
@@ -1,4 +1,20 @@
 var hasCycle = function (head) {
+    let slowRunner = head;
+    let fastRunner = head;
+
+    while (fastRunner && fastRunner.next) {
+        slowRunner = slowRunner.next;
+        fastRunner = fastRunner.next.next;
+
+        if (slowRunner === fastRunner) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+var hasCycle = function (head) {
     const seenNodes = new Set();
 
     let currentNode = head;
