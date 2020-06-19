f10486ecd9ac4cdc791caf510798daecbc24dc39
Ajay <ajayvishwanath.94@gmail.com>
2020-06-19 15:19:29 -0700

    reverse linked list

---
+++ Linked Lists/reverse_linked_list.js
@@ -0,0 +1,35 @@
+//iterative solution
+var reverseList = function (head) {
+  let currentNode = head;
+  let prevNode = null;
+  let nextNode = null;
+
+  while (currentNode) {
+    nextNode = currentNode.next;
+    currentNode.next = prevNode;
+    prevNode = currentNode;
+    currentNode = nextNode;
+  }
+
+  return prevNode;
+};
+
+//recursive solution
+let result;
+
+reverseList = function (head) {
+  if (!head) {
+    return head;
+  }
+
+  if (!head.next) {
+    result = head;
+    return head;
+  }
+
+  reverseList(head.next);
+  head.next.next = head;
+  head.next = undefined;
+
+  return result;
+};
